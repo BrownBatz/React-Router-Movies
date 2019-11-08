0f391f7993e7258394932d6db11201f95af6bf54
Gabriel Lopez <gabelopez@Claires-MacBook-Pro.local>
2019-11-07 19:34:24 -0700

    I have hit MVP for this project

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,22 +3,23 @@ import axios from 'axios';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
- 
+  console.log('Running use effect now');
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
         .get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
+          console.log('It ran');
           setMovie(response.data);
         })
         .catch(error => {
           console.error(error);
         });
-
-  },[]);
+    console.log('It ran');
+  }, []);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
@@ -43,7 +44,7 @@ const Movie = (props) => {
         </div>
         <h3>Actors</h3>
 
-        {stars.map(star => (
+        {stars && stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
